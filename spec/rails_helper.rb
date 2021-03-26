2e81534c33f8e9fac14d83a62b9c0d69853e01af
Luan Ribeiro <luan.mesquita@hotmail.com>
2021-03-26 01:27:48 -0300

    Devise Implemented

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,9 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
